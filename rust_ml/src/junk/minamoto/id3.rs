85b3d7afb211d9a05788aca0781f47ed29df4126
Vasily Levchenko <minamoto@yandex-team.ru>
2016-01-19 10:27:50 +0300

    bit more ID3

--- rust_ml/src/junk/minamoto/id3.rs
+++ rust_ml/src/junk/minamoto/id3.rs
@@ -1,9 +1,17 @@
 use super::tree;
 use super::entropy;
 
+use std::fmt::Display as Display;
+use std::fmt::Debug as Debug;
 
-pub fn create_tree<C:PartialEq, T:entropy::Classified<C>>(_dataset:&Vec<(T, C)>, _classes:Vec<C>)
+pub fn create_tree<C:PartialEq+Clone+Debug, T:entropy::Classified<C>+Display+Clone>(dataset:&Vec<(T, C)>, classes:&Vec<C>)
                                                           -> Box<tree::Node<T>> {
-    
+    let set:Vec<T> = dataset.iter().map(|x| x.0.clone()).collect();
+    let entropies:Vec<(C, f64)> = classes.iter().map(|c| (c.clone(), entropy::gain(&set, &classes, c))).collect();
+
+    for s in entropies {
+        println!("{:?}", s)
+    }
+
     Box::new(tree::Node{data:None, left:None, rigth:None})
 }
