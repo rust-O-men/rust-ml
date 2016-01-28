e48279d140c85bd05f00e3ee86ba758bd742fae4
ijnikiforov <igor.j.nikiforov@gmail.com>
2016-01-28 22:04:03 +0300

    remove DataSetMeta

--- rust_ml/src/junk/ijnikiforov/entropy.rs
+++ rust_ml/src/junk/ijnikiforov/entropy.rs
@@ -5,7 +5,10 @@ mod api {
 	pub type Number = f64;
 	pub type Category = u32;
 
-	pub type DataSet<T> = Vec<(T, Target)>;
+	pub struct DataSet<T> {
+		pub records: Vec<(T, Target)>,
+		pub target_count: usize
+	}
 
 	pub enum FeatureType {
 		Boolean,
@@ -24,11 +27,6 @@ mod api {
 
 	}
 
-	pub trait DataSetMeta {
-
-		fn target_count(&self) -> usize;
-	    
-	}
 
 }
 
@@ -38,8 +36,8 @@ mod data {
 
 	pub struct Record {
 		pub category: api::Category,
-    	pub number: api::Number,	
-    	pub boolean: bool
+		pub number: api::Number,	
+		pub boolean: bool
 	}
 
 	impl api::RecordMeta for Record {
@@ -50,57 +48,52 @@ mod data {
 
 		fn feature_name(&self, feature: usize) -> String {
 			match feature {
-			    0 => "category".to_string(),
-			    1 => "number".to_string(),
-			    2 => "boolean".to_string(),
-			    _ => panic!("Unknown feature")
+				0 => "category".to_string(),
+				1 => "number".to_string(),
+				2 => "boolean".to_string(),
+				_ => panic!("Unknown feature")
 			}
 		}
 
 		fn feature_type(&self, feature: usize) -> api::FeatureType {
 			match feature {
-			    0 => api::FeatureType::Category,
-			    1 => api::FeatureType::Number,
-			    2 => api::FeatureType::Boolean,
-			    _ => panic!("Unknown feature")
+				0 => api::FeatureType::Category,
+				1 => api::FeatureType::Number,
+				2 => api::FeatureType::Boolean,
+				_ => panic!("Unknown feature")
 			}	
 		}
 
 		fn category_count(&self, feature: usize) -> usize {
 			match feature {
-			    0 => 3,
-			    _ => panic!("Unknown feature")
-			}	
+				0 => 3,
+				_ => panic!("Unknown feature")
+			}
 
-		}
-
-	}
-
-	impl api::DataSetMeta for api::DataSet<Record> {
-
-		fn target_count(&self) -> usize {
-			2
 		}
 
 	}
 
 	pub fn read_data() -> api::DataSet<Record> {
-		let result = vec![
-			(Record{category: 0, number: 70.0, boolean: true }, 0),
-			(Record{category: 0, number: 90.0, boolean: true }, 1),
-			(Record{category: 0, number: 85.0, boolean: false}, 1),
-			(Record{category: 0, number: 95.0, boolean: false}, 1),
-			(Record{category: 0, number: 70.0, boolean: false}, 0),
-			(Record{category: 1, number: 90.0, boolean: true }, 0),
-			(Record{category: 1, number: 78.0, boolean: false}, 0),
-			(Record{category: 1, number: 65.0, boolean: true }, 0),
-			(Record{category: 1, number: 75.0, boolean: false}, 0),
-			(Record{category: 2, number: 80.0, boolean: true }, 1),
-			(Record{category: 2, number: 70.0, boolean: true }, 1),
-			(Record{category: 2, number: 80.0, boolean: false}, 0),
-			(Record{category: 2, number: 80.0, boolean: false}, 0),
-			(Record{category: 2, number: 96.0, boolean: false}, 0)
-		];
+		let result = api::DataSet{
+			records: vec![
+				(Record{category: 0, number: 70.0, boolean: true }, 0),
+				(Record{category: 0, number: 90.0, boolean: true }, 1),
+				(Record{category: 0, number: 85.0, boolean: false}, 1),
+				(Record{category: 0, number: 95.0, boolean: false}, 1),
+				(Record{category: 0, number: 70.0, boolean: false}, 0),
+				(Record{category: 1, number: 90.0, boolean: true }, 0),
+				(Record{category: 1, number: 78.0, boolean: false}, 0),
+				(Record{category: 1, number: 65.0, boolean: true }, 0),
+				(Record{category: 1, number: 75.0, boolean: false}, 0),
+				(Record{category: 2, number: 80.0, boolean: true }, 1),
+				(Record{category: 2, number: 70.0, boolean: true }, 1),
+				(Record{category: 2, number: 80.0, boolean: false}, 0),
+				(Record{category: 2, number: 80.0, boolean: false}, 0),
+				(Record{category: 2, number: 96.0, boolean: false}, 0)
+			],
+			target_count: 2
+		};
 		result
 	}
 
@@ -111,16 +104,16 @@ mod solve_func {
 
 	use ::api;
 
-	pub fn information_gain<T: api::RecordMeta, F>(data: &api::DataSet<T>, criterion: &F, target_count: usize) -> f64 where F: Fn(&T) -> bool {
-		let e = target_entropy(data, target_count);
-		let fe = entropy(data, criterion, target_count);
+	pub fn information_gain<T: api::RecordMeta, F>(data: &api::DataSet<T>, criterion: &F) -> f64 where F: Fn(&T) -> bool {
+		let e = target_entropy(data);
+		let fe = entropy(data, criterion);
 		e - fe
 	}
 
-	fn entropy_helper<T: api::RecordMeta, F>(data: &api::DataSet<T>, value: bool, criterion: &F, target_count: usize) -> f64 where F: Fn(&T) -> bool {
+	fn entropy_helper<T: api::RecordMeta, F>(data: &api::DataSet<T>, value: bool, criterion: &F) -> f64 where F: Fn(&T) -> bool {
 		let mut total = 0;
-		let mut classes = vec![0; target_count];
-		for record in data.iter() {
+		let mut classes = vec![0; data.target_count];
+		for record in data.records.iter() {
 			if criterion(&record.0) == value {
 				classes[record.1 as usize] += 1;
 				total += 1;
@@ -131,16 +124,16 @@ mod solve_func {
 			if *class != 0 {	
 				let p = (*class as f64) / (total as f64);
 				result += p * p.log2()
-			} 
+			}
 		}
 		-result
 	}
 
-	fn entropy<T: api::RecordMeta, F>(data: &api::DataSet<T>, criterion: &F, target_count: usize) -> f64 where F: Fn(&T) -> bool {
-		let total = data.len();
+	fn entropy<T: api::RecordMeta, F>(data: &api::DataSet<T>, criterion: &F) -> f64 where F: Fn(&T) -> bool {
+		let total = data.records.len();
 		let mut ccs = vec![0u8, 0u8];
 		let values = vec![false, true];
-		for record in data.iter() {
+		for record in data.records.iter() {
 			let i = match criterion(&record.0) {
 					false => 0,
 					true => 1,
@@ -150,15 +143,15 @@ mod solve_func {
 		let mut result = 0f64;
 		for i in 0..2 {
 			let p = (ccs[i] as f64) / (total as f64);
-			result += p * entropy_helper(data, values[i], criterion, target_count); 
+			result += p * entropy_helper(data, values[i], criterion); 
 		}
 		result
 	}
 
-	fn target_entropy<T: api::RecordMeta>(data: &api::DataSet<T>, target_count: usize) -> f64 {
-		let total = data.len();
-		let mut classes = vec![0; target_count];
-		for record in data.iter() {
+	fn target_entropy<T: api::RecordMeta>(data: &api::DataSet<T>) -> f64 {
+		let total = data.records.len();
+		let mut classes = vec![0; data.target_count];
+		for record in data.records.iter() {
 			classes[record.1 as usize] += 1;
 		}
 		for class in classes.iter() {
@@ -172,15 +165,13 @@ mod solve_func {
 			result += p * p.log2()
 		}
 		-result
-	} 
+	}
 
 }
 
 fn main() {
-	use api::DataSetMeta;
-
-    println!("Hello, ID3!");
-    let data = data::read_data(); 
-    let entropy = solve_func::information_gain(&data, &|rec| rec.boolean, data.target_count());  
-    println!("{}", entropy); 
+	println!("Hello, ID3!");
+	let data = data::read_data(); 
+	let entropy = solve_func::information_gain(&data, &|rec| rec.boolean);  
+	println!("{}", entropy); 
 }
