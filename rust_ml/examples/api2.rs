000f0bc37fc3a1a3ea52d95fb04f61f022433252
ijnikiforov <igor.j.nikiforov@gmail.com>
2016-02-14 12:27:45 +0300

    DataSetView draft added to API draft

--- rust_ml/examples/api2.rs
+++ rust_ml/examples/api2.rs
@@ -1,6 +1,8 @@
 
 mod api {
 
+    use std::collections::HashSet;
+
     pub type Target = u32;
 
     pub enum FeatureType {
@@ -28,12 +30,37 @@ mod api {
     pub struct DataSetIterator<'a, T: 'a> {
         records: &'a Vec<(T, Target)>,
         current: usize,
-        max: usize
+        max: usize,
+        view: Option<&'a DataSetView>
     } 
+
+    pub struct DataSetView {
+        indexes: HashSet<usize> // bitset should be here
+    }
+
+    impl DataSetView {
+
+        pub fn empty() -> Self {
+            DataSetView{indexes: HashSet::new()}
+        }
+
+        pub fn new(indexes: Vec<usize>) -> Self {
+            let mut is = HashSet::new();
+            for index in indexes {
+                is.insert(index);
+            }
+            DataSetView{indexes: is}
+        }
+
+        pub fn add_index(&mut self, index: usize) {
+            self.indexes.insert(index);
+        }
+
+    }
 
     impl<T: RecordMeta> DataSet<T> {
 
-        pub fn new(meta: DataSetMeta, records: Vec<(T, Target)>) -> DataSet<T> {
+        pub fn new(meta: DataSetMeta, records: Vec<(T, Target)>) -> Self {
             // here we should check that all records has the same meta
             DataSet{records: records, meta: meta}
         }
@@ -42,12 +69,20 @@ mod api {
             self.records.is_empty()
         }
 
+        pub fn len(&self) -> usize {
+            self.records.len()
+        }        
+
         pub fn target_count(&self) -> usize {
             self.meta.target_count
         }
 
         pub fn records(&self) -> DataSetIterator<T> {
-            DataSetIterator{records: &self.records, current: 0, max: self.records.len()}
+            DataSetIterator{records: &self.records, current: 0, max: self.records.len(), view: None}
+        }
+
+        pub fn view_records<'a>(&'a self, view: &'a DataSetView) -> DataSetIterator<T> {
+            DataSetIterator{records: &self.records, current: 0, max: self.records.len(), view: Some(view)}    
         }
 
     }
@@ -58,6 +93,14 @@ mod api {
 
         fn next(&mut self) -> Option<&'a (T, Target)> {
             if self.current < self.max {
+                match self.view {
+                    Some(ref view) => {
+                        while view.indexes.contains(&self.current) {
+                            self.current = self.current + 1;    
+                        };
+                    }
+                    Node => {}
+                }
                 let result = Some(&self.records[self.current]);
                 self.current = self.current + 1;
                 return result
@@ -113,6 +156,17 @@ fn test<T: api::RecordMeta>(ds: &api::DataSet<T>) {
     }    
 }
 
+fn test2<T: api::RecordMeta>(ds: &api::DataSet<T>, view: &mut api::DataSetView, index: usize) {
+    if ds.len() == index {
+        return
+    }
+    for record in ds.view_records(view) {
+        println!("{}: {}", index, record.0.some_record_meta());
+    }
+    view.add_index(index);
+    test2(ds, view, index + 1)    
+}
+
 fn main() {
     use data;
     println!("Hello, API!");
@@ -121,5 +175,9 @@ fn main() {
     for record in ds.records() {
         println!("{:?}", record);
     }
+    for record in ds.view_records(&api::DataSetView::new(vec![0])) {
+        println!("{:?}", record);
+    }
     test(&ds);
+    test2(&ds, &mut api::DataSetView::empty(), 0);
 }
