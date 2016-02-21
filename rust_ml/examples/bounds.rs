1c41aa83efb8248245290e0c1f49278aefd97ab1
ijnikiforov <igor.j.nikiforov@gmail.com>
2016-02-21 17:51:41 +0300

    vectorization draft

--- rust_ml/examples/bounds.rs
+++ rust_ml/examples/bounds.rs
@@ -2,13 +2,24 @@ extern crate rust_ml;
 
 use rust_ml::api;
 use rust_ml::features::bounds;
+use rust_ml::features::vectorize;
 use rust_ml::dataset::iris_flower;
+use rust_ml::dataset::simple;
 
 fn main() {
-	println!("Hello, bounds!");
-	let data = iris_flower::read_data(); 
-	let bounds1 = bounds::uniform_selector(&data, 0 as api::Feature, 5usize);
-	println!("{:?}", bounds1);
-	let _ = bounds::bounds_criterions(&data, &bounds::uniform_selector, 5usize);
+    println!("Hello, bounds and vectorize!");
+    let iris = iris_flower::read_data(); 
+    let bounds1 = bounds::uniform_selector(&iris, 0 as api::Feature, 5usize);
+    println!("{:?}", bounds1);
+    let _ = bounds::bounds_criterions(&iris, &bounds::uniform_selector, 5usize);
+    for record in iris.records.iter() {
+        let vectorized = vectorize::vectorize(&record.0);
+        println!("{:?}", vectorized);
+    }
+    let simple = simple::read_data();
+    for record in simple.records.iter() {
+        let vectorized = vectorize::vectorize(&record.0);
+        println!("{:?}", vectorized);
+    }
 }
 
---
+++ rust_ml/src/features/vectorize.rs
@@ -0,0 +1,32 @@
+use super::super::api;
+
+pub fn vectorize<T: api::RecordMeta>(record: &T) -> Vec<api::Number> {
+    let mut result = Vec::new();
+    for f in 0..record.feature_count() {
+        match record.feature_type(f) {
+            api::FeatureType::Number => {
+                match record.number_value(f) {
+                    Some(value) => result.push(value),
+                    None => {},
+                };
+            },
+            api::FeatureType::Boolean => {
+                match record.bool_value(f) {
+                    Some(value) => result.push(if value {0.0} else {1.0}),
+                    None => {},
+                };
+            },
+            api::FeatureType::Category => {
+                match record.category_value(f) {
+                    Some(value) => {
+                        for v in 0..record.category_count(f) {
+                            result.push(if v as u32 == value {1.0} else {0.0});
+                        };
+                    },
+                    None => {},
+                };  
+            }
+        };
+    }
+    result
+}
