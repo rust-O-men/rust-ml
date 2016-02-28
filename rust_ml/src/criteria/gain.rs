8d65ce0dd6d030b0964fdab783ee765e30a94781
Vasily Levchenko <vasily.v.levchenko@gmail.com>
2016-02-28 18:14:57 +0300

    no-view: src/criteria/gain.rs: uglified gain but in new api (not final stage 'cource)

--- rust_ml/src/criteria/gain.rs
+++ rust_ml/src/criteria/gain.rs
@@ -6,62 +6,26 @@ pub fn information_gain<T: api::RecordMeta>(data: &api::DataSet<T>, criterion: &
 	e - fe
 }
 
-fn entropy_helper<T: api::RecordMeta>(data: &api::DataSet<T>, view: &api::DataSetView, criterion: &api::Criterion<T>) -> f64 {
-	let mut total = 0;
-	let mut classes = vec![0; data.target_count];
-	for index in view.iter() {
-		let record = &data.records[*index];
-		if criterion(&record.0) == value {
-			classes[record.1 as usize] += 1;
-			total += 1;
-		}
-	}
-	let mut result = 0f64;
-	for class in classes.iter() {
-		if *class != 0 {	
-			let p = (*class as f64) / (total as f64);
-			result += p * p.log2()
-		}
-	}
-	-result
+fn entropy_helper<T: api::RecordMeta>(data: &api::DataSet<T>) -> f64 {
+    let total = data.total_count() as f64;
+    let classes = data.class_count();
+    
+    let mut result = 0f64;
+    for (k, v) in classes.iter() {
+	let p = (*v as f64) / total;
+	result += p * p.log2()
+    }
+    -result
 }
 
-fn entropy<T: api::RecordMeta>(data: &api::DataSet<T>, view: &api::DataSetView, criterion: &api::Criterion<T>) -> f64 {
-	let total = data.records.len();
-	let mut ccs = vec![0u8, 0u8];
-	let values = vec![false, true];
-	for index in view.iter() {
-		let record = &data.records[*index];	
-		let i = match criterion(&record.0) {
-				false => 0,
-				true => 1,
-		};
-		ccs[i] += 1;
-	}
-	let mut result = 0f64;
-	for i in 0..2 {
-		let p = (ccs[i] as f64) / (total as f64);
-		result += p * entropy_helper(data, view, values[i], criterion); 
-	}
-	result
+fn entropy<T: api::RecordMeta>(data: &api::DataSet<T>, criterion: &api::Criterion<T>) -> f64 {
+    let total = data.total_count();
+    let ds = data.iter().filter(|x| criterion(&x.0)).cloned().collect();
+    let mut result = 0f64;
+    entropy_helper(&ds)
 }
 
 fn target_entropy<T: api::RecordMeta>(data: &api::DataSet<T>) -> f64 {
-	let total = data.records.len();
-	let mut classes = vec![0; data.target_count];
-	for index in view.iter() {
-		let record = &data.records[*index];	
-		classes[record.1 as usize] += 1;
-	}
-	for class in classes.iter() {
-		if *class == 0 {
-			return 0f64	
-		}
-	}
 	let mut result = 0f64;
-	for class in classes.iter() {
-		let p = (*class as f64) / (total as f64);
-		result += p * p.log2()
-	}
 	-result
 }
