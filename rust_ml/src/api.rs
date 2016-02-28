d46040ef2f200e6e4ee53572d00c4fbc3acc0af7
Vasily Levchenko <vasily.v.levchenko@gmail.com>
2016-02-28 18:13:38 +0300

    no-view: new view on api (the most valuable mods are off)

--- rust_ml/src/api.rs
+++ rust_ml/src/api.rs
@@ -4,13 +4,18 @@ pub type Category = u32;
 pub type Feature = usize;
 
 use std::rc::Rc;
+use std::collections::HashSet;
+use std::collections::HashMap;
+use std::iter::FromIterator;
+use std::iter::IntoIterator;
+use std::slice::Iter;
 
-pub struct DataSet<T: RecordMeta> {
-    pub records: Vec<(T, Target)>,
-    pub target_count: usize
-}
+/* TODO: perhaps Target should be optional */
+pub struct DataSet<T:RecordMeta>(Vec<Rc<(T, Target)>>, HashSet<DataSetMeta>);
+#[derive(PartialEq, Eq, Hash)]
+pub struct DataSetMeta(Target);
 
-#[derive(PartialEq, Eq)]
+#[derive(PartialEq, Eq, Hash)]
 pub enum FeatureType {
     Boolean,
     Category,
@@ -18,7 +23,6 @@ pub enum FeatureType {
 }
 
 pub trait RecordMeta {
-        
     fn feature_count(&self) -> usize;
 
     fn feature_name(&self, feature: Feature) -> String;
@@ -33,10 +37,64 @@ pub trait RecordMeta {
 
 pub type Criterion<T> = Fn(&T) -> bool;
 
-pub type Solver<T> = Fn(&Vec<Rc<(T, u32)>>, &Criterion<T>) -> f64;
+pub type Solver<T> = Fn(&DataSet<T>, &Criterion<T>) -> f64;
 
-pub type DataSetView = Vec<usize>;
+impl<T:RecordMeta> DataSet<T> {
+    pub fn new() -> DataSet<T> {
+        let v:Vec<Rc<(T, Target)>> = Vec::new();
+        DataSet(v, HashSet::new())
+    }
 
-pub fn all_view<T: RecordMeta>(data: &DataSet<T>) -> DataSetView {
-    (0usize..data.records.len()).collect()
+    pub fn add(&mut self, item:T, target:Target) {
+        self.0.push(Rc::new((item, target)));
+        self.1.insert(DataSetMeta::new(target));
+    }
+
+    pub fn add_tuple(&mut self, item:Rc<(T, Target)>) {
+        self.0.push(item.clone());
+        self.1.insert(DataSetMeta::new(item.1));
+    }
+    
+    pub fn class_count(&self) -> HashMap<Target, usize> {
+        let mut cs = HashMap::new();
+        for r in &self.0 {
+            let counter = cs.entry(r.1).or_insert(0);
+            *counter += 1;
+        }
+        cs
+    }
+
+    pub fn iter(&self) -> Iter<Rc<(T, Target)>> {
+        self.0.iter()
+    }
+
+    pub fn total_count(&self) -> usize {
+        self.0.len()
+    }
 }
+
+impl DataSetMeta {
+    pub fn new(t:Target) -> DataSetMeta{
+        DataSetMeta(t)
+    }
+}
+
+impl<T:RecordMeta> FromIterator<Rc<(T,Target)>> for DataSet<T> {
+    fn from_iter<I: IntoIterator<Item=Rc<(T, Target)>>> (iterator:I) -> Self {
+        let mut ds = DataSet::new();
+        for i in iterator {
+            ds.add_tuple(i.clone());
+        }
+        ds
+    }
+}
+
+impl<T:RecordMeta> IntoIterator for DataSet<T> {
+    type Item = Rc<(T, Target)>;
+    type IntoIter = ::std::vec::IntoIter<Rc<(T,Target)>>;
+    
+    fn into_iter (self) -> Self::IntoIter {
+        self.0.into_iter()
+    }
+}
+
--- rust_ml/src/lib.rs
+++ rust_ml/src/lib.rs
@@ -1,10 +1,10 @@
 pub mod junk;
 pub mod api;
 pub mod criteria;
-pub mod dataset;
-pub mod trees;
-pub mod features;
-pub mod gradient;
+//pub mod dataset;
+//pub mod trees;
+//pub mod features;
+//pub mod gradient;
 
 #[test]
 fn it_works() {
