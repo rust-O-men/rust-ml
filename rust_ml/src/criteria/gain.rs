fef1079bc5904aef184783c5c834937cfe3c3019
Vasily Levchenko <vasily.v.levchenko@gmail.com>
2016-02-28 20:08:14 +0300

    no-view: src/criteria/gain.rs: should works as ijnikiforov designed

--- rust_ml/src/criteria/gain.rs
+++ rust_ml/src/criteria/gain.rs
@@ -11,7 +11,7 @@ fn entropy_helper<T: api::RecordMeta>(data: &api::DataSet<T>) -> f64 {
     let classes = data.class_count();
     
     let mut result = 0f64;
-    for (k, v) in classes.iter() {
+    for (_, v) in classes.iter() {
 	let p = (*v as f64) / total;
 	result += p * p.log2()
     }
@@ -19,13 +19,19 @@ fn entropy_helper<T: api::RecordMeta>(data: &api::DataSet<T>) -> f64 {
 }
 
 fn entropy<T: api::RecordMeta>(data: &api::DataSet<T>, criterion: &api::Criterion<T>) -> f64 {
-    let total = data.total_count();
-    let ds = data.split_positive(criterion);
-    let mut result = 0f64;
-    entropy_helper(&ds)
+    let total = data.total_count() as f64;
+    let (ls,rs) = data.split(criterion);
+    (ls.total_count() as f64)/total * entropy_helper(&ls)
+        + (rs.total_count() as f64)/total * entropy_helper(&rs)
 }
 
 fn target_entropy<T: api::RecordMeta>(data: &api::DataSet<T>) -> f64 {
-	let mut result = 0f64;
-	-result
+    let classes = data.class_count(); 
+    let total = data.total_count() as f64;
+    let mut result = 0f64;
+    for (_, v) in classes {
+        let p = (v as f64)/total;
+        result += p * p.log2();
+    }
+    -result
 }
